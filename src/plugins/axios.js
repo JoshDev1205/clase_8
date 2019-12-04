c67ebe5984210f0db40f51bb4ae4b9d3cb29fab4
Joseph Ospina Tafur <josh.999126@gmail.com>
2019-12-04 12:54:36 -0500

    Configuracion de Vuetify y Axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,61 +1,30 @@
-"use strict";
+'use strict'
 
-import Vue from 'vue';
-import axios from "axios";
-
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+import Vue from 'vue'
+import axios from 'axios'
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
-};
+  baseURL: process.env.VUE_APP_APIURL || '',
+}
 
-const _axios = axios.create(config);
-
-_axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
-
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
-
+export const httpClient = axios.create(config)
 Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
+  Vue.axios = httpClient
+  window.axios = httpClient
   Object.defineProperties(Vue.prototype, {
     axios: {
       get() {
-        return _axios;
-      }
+        return httpClient
+      },
     },
     $axios: {
       get() {
-        return _axios;
-      }
+        return httpClient
+      },
     },
-  });
-};
+  })
+}
 
 Vue.use(Plugin)
 
-export default Plugin;
+export default Plugin

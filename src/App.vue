737b0997ea3fd07d490a0ac49f31933b215a4637
Joseph Ospina Tafur <josh.999126@gmail.com>
2019-12-21 01:17:40 -0500

    clase 8.6

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
 	<v-app>
 		<v-content>
-			<router-view />
+			<v-app-bar
+				dark
+				class="light-blue darken-2"
+			>
+				<v-toolbar-title>EdBookstore</v-toolbar-title>
+				<v-spacer />
+				<v-toolbar-items>
+					<v-btn text>Inicio</v-btn>
+					<v-btn text>Autores</v-btn>
+					<v-btn text>Libros</v-btn>
+				</v-toolbar-items>
+			</v-app-bar>
+			<v-container>
+				<router-view />
+			</v-container>
 		</v-content>
 	</v-app>
 </template>
